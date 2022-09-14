d538c8a0ecba72bd38f743f3de73ccd17abb2c7e
Ruba Sbeih <rsasbeih@gmail.com>
2022-09-14 23:07:16 +0300

    finished task 1 of module 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+        timerId = setInterval(changeBodyColor, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+        clearInterval(timerId);
+});
+
+function changeBodyColor() {
+        document.body.style.backgroundColor = getRandomHexColor();
+}
